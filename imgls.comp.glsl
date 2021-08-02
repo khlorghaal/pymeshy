ac61529568d9741c00e750a668d4a6bfdf6f30e0
khlorghaal <khlorghaal@live.com>
2021-08-02 18:22:27 -0400

    stages, i dont like

--- imgls.comp.glsl
+++ imgls.comp.glsl
@@ -1,16 +1,3 @@
-#version 450
-
-layout(
-	local_size_x= 2,//dfdxy
-	local_size_y= 2,
-	local_size_z= 1
-	) in;
-
-layout(binding=0, location=0, rgba32f) restrict uniform image2D img;
-//GL_READ_WRITE
-//format=rgba16f
-
-
 #define vec1 float
 #define ivec1 int
 #define uvec1 uint
@@ -20,8 +7,6 @@ layout(binding=0, location=0, rgba32f) restrict uniform image2D img;
 #define sat saturate
 #define sats saturate_signed
 
-
-
  vec1 v_i_f(ivec1 v){return  vec1(v);}
  vec2 v_i_f(ivec2 v){return  vec2(v);}
  vec3 v_i_f(ivec3 v){return  vec3(v);}
@@ -30,7 +15,6 @@ ivec1 v_f_i( vec1 v){return ivec1(v);}
 ivec2 v_f_i( vec2 v){return ivec2(v);}
 ivec3 v_f_i( vec3 v){return ivec3(v);}
 ivec4 v_f_i( vec4 v){return ivec4(v);}
-
 
 float sum ( vec2 v){ return dot(v,vec2(1));}
 float sum ( vec3 v){ return dot(v,vec3(1));}
@@ -66,24 +50,79 @@ float prod( vec4 v){ return v.x*v.y*v.z*v.w;}
 
 float vnse_2i_1f(ivec2 p){return nmapu(hash_i_f(hash_i_i(p.x)+hash_i_i(p.y)));}
 
+#define bilerp(st,nn,np,pn,pp) \
+	lerp(\
+		lerp(nn,pn,st.x),\
+		lerp(np,pp,st.x),\
+		st.y)
+
+
+
+
+
+
+layout(binding=0, location=0, rgba16f) readonly  restrict uniform image2D img_i;
+layout(binding=1, location=1, rgba16f) writeonly restrict uniform image2D img_o;
+
+//"Store operations to any texel that is outside the boundaries of the bound image will do nothing."
+layout(
+		local_size_x= 8,//dfdxy
+		local_size_y= 8,
+		local_size_z= 1
+		) in;
+
+#ifndef STAGE0
+	shared vec4 sh[8][8];
+#endif
+
 void main(){
+	uvec2 ires= imageSize(img_i);
+	vec2 res= vec2(ires);
 	ivec2 iuv= ivec2(gl_GlobalInvocationID.xy);
-	vec4 bb= imageLoad(img, iuv);
-	uvec2 wh= imageSize(img);
-	vec2 uv= vec2(iuv)/vec2(wh);
+	 vec2  uv=  vec2(iuv)/res;
 
-	float h= vnse_2i_1f(iuv);
-	h= step(h,.01);//star concentration
+	vec4 col;
 
-	float m= vnse_2i_1f(iuv+INT_HALFMAX);
-	m= exp(-m*m*.95);//magnitude distribution
+	#ifdef STAGE0
 
-	float l= m*h;
+		float h= vnse_2i_1f(iuv);
+		h= step(h,.01);//star concentration
 
-	vec4 col= vec4(0.,uv,1.);
+		float m= vnse_2i_1f(iuv+INT_HALFMAX);
+		m= exp(-m*m*.98);//magnitude distribution
 
-	col.rgb= vec3(l);
+		float l= m*h;
 
-	barrier();
-	imageStore(img, iuv, col);
+		col= vec4(vec3(l),1.);//vec4(0.,uv,1.);
+
+	#elif STAGE1
+		uvec2 gid= gl_GlobalInvocationID.xy;
+		uvec2 lid=  gl_LocalInvocationID.xy;
+		uvec2 lsz= gl_WorkGroupSize.xy;
+		sh[lid.x][lid.y]= imageLoad(img_i,ivec2(gid));
+		barrier();
+		{
+			vec2 uv= uv;
+
+			uv= uv+ .1;
+
+			ivec2 iuv= ivec2(uv*res);
+			vec2 st= fract(uv*res);
+			uvec2 i0= iuv;
+			uvec2 i1= i0+1;
+			vec4 nn= sh[i0.x][i0.y];
+			vec4 np= sh[i0.x][i1.y];
+			vec4 pn= sh[i1.x][i0.y];
+			vec4 pp= sh[i1.x][i1.y];
+			vec4 samp= bilerp(st,nn,np,pn,pp);
+		}
+		col= vec4(0.,uv,1.);
+	#elif STAGE2
+		col= vec4(0.,uv,1.);
+
+	#else
+		#error no stage #defined
+	#endif
+
+	imageStore(img_o, iuv, col);
 }
--- imgls.py
+++ imgls.py
@@ -1,8 +1,10 @@
 import numpy as np
-from PIL import Image
+import png
+#import exr
+
 img= None
 try:
-	img= Image.open('img.png')
+	img= png.Reader('img.png').read()
 	w= img.width
 	h= img.height
 	rast= img.raster.flatten()
@@ -26,51 +28,64 @@ resolution= (w,h)
 pygame.display.set_mode(resolution, DOUBLEBUF | OPENGL)
 pygame.display.set_caption('____________________________')
 
-prog= None
 with open("imgls.comp.glsl") as f:
 	csh_src= f.read()
-try:
-	csh_sh= shaders.compileShader(csh_src, GL_COMPUTE_SHADER)
-	prog= shaders.compileProgram(csh_sh)
-except Exception as e:
-	e= str(e)
-	e= e.replace('\\\\n','\n')
-	e= e.replace(  '\\n','\n')
-	e= e.replace(  '\\t','\t')
-	e= e.replace(  '\\t','\t')
-	e= e.replace(  '\\','')
-	e= e.replace('\\\\','')#fuck
-	import re
-	e= re.sub(r'\(([0-9]+)\)', r'\nFile "imgls.comp.glsl", line \1', e)
-	print('\nSHADERROR\n')
-	print(e)
-	exit()
+def prog(s):
+	try:
+		head= '''
+		#version 450
+		#define %s 1
+		#line 1
+		'''%s
+		csh_sh= shaders.compileShader(head+csh_src, GL_COMPUTE_SHADER)
+		return shaders.compileProgram(csh_sh)
+	except Exception as e:
+		print('\nSHADERROR\n')
+		e= str(e)
+		e= e.replace('\\\\n','\n')
+		e= e.replace(  '\\n','\n')
+		e= e.replace(  '\\t','\t')
+		e= e.replace(  '\\t','\t')
+		e= e.replace(  '\\','')
+		e= e.replace('\\\\','')#fuck
+		import re
+		e= re.sub(r'\(([0-9]+)\)', r'\nFile "imgls.comp.glsl", line \1', e)
+		#sublime default error regex
+		print(e)
+		exit()
+progs= list(map(prog,['STAGE0','STAGE1','STAGE2']))
+print(progs)
 
-tex= glGenTextures(1)
-glBindTexture(GL_TEXTURE_2D,tex)
-glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA16F, w,h)#uninitialized
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-glTexSubImage2D(GL_TEXTURE_2D, 0,0,0,w,h, GL_RGBA,GL_FLOAT, rast);
-glBindImageTexture(0,tex, 0,False,0, GL_READ_WRITE, GL_RGBA16F)
+_pingpong= glGenTextures(2)
+for i,pp in enumerate(_pingpong):
+	glBindTexture(GL_TEXTURE_2D,pp)
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA16F, w,h)#uninitialized
+	#glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+	#glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+	glTexSubImage2D(GL_TEXTURE_2D, 0,0,0,w,h, GL_RGBA,GL_FLOAT, rast);
 
 
-glUseProgram(prog)
-#texsubdata is coherent glMemoryBarrier( GL_TEXTURE_UPDATE_BARRIER_BIT )
-glDispatchCompute(w//2,h//2,1)
-glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT )
-del rast
-rast= glGetTexImage(GL_TEXTURE_2D, 0,GL_RGBA,GL_FLOAT)
+wg= (w//8,h//8,1)
+for p in progs:
+	glUseProgram(p)
+
+	_pingpong= _pingpong[::-1]
+	glBindImageTexture(0,_pingpong[0], 0,False,0, GL_READ_ONLY,  GL_RGBA16F)
+	glBindImageTexture(1,_pingpong[1], 0,False,0, GL_WRITE_ONLY, GL_RGBA16F)
+
+	glDispatchCompute(*wg)
+	glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT )
 
 fb= glGenFramebuffers(1)
 glBindFramebuffer(GL_READ_FRAMEBUFFER, fb)
 glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0)
-glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,tex, 0)
+glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,_pingpong[1], 0)
 glBlitFramebuffer(0,0,w,h,0,0,w,h,GL_COLOR_BUFFER_BIT, GL_NEAREST)
+del rast
+rast= glReadPixels(0,0,w,h, GL_RGBA,GL_FLOAT)
 
 pygame.display.flip()
 
-import png
 print(rast.shape)
 rast= rast*(-1+2**16)
 rast= rast.astype(np.uint16).flatten()
@@ -80,10 +95,12 @@ w= png.Writer(
 	bitdepth=16,#!!16
 	greyscale=False,
 	alpha= True,
-	compression=9
+	compression=8
 	)
-w.write_array( open('./out.png','wb'), rast )
-#img= png.fromarray(rast,mode="RGBA").save('./out.png')
 
-from time import sleep
-sleep(1.) 
+w.write_array( open('./out.png','wb'), rast )
+
+#while(1):
+#	for event in pygame.event.get():
+#            if event.type == pygame.QUIT:
+#                exit()
